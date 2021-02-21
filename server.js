83d11f44709e466729b90f2b7ef447b38d9ff068
abbotkinneydude <pa@soundtrackspecialist.com>
2021-02-20 19:41:18 -0800

    updated server.js with heroku settings, MONGODB_URI, <useUnifiedTopology: true> for new Server Discover and Monitoring engine

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = 4000;
 
 const app = express();
 
@@ -15,14 +15,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// Added MONGODB_URI for Heroku Deployment + <useUnifiedTopology: true> for new Server Discover and Monitoring engine.
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
+// Added ENV port for Heroku deployment
+
+app.listen(process.env.PORT || 6502, () => {
   console.log(`App running on port ${PORT}!`);
 });
